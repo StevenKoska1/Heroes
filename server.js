7ace89b09dbb42b0ade25393370d3fde7153cd4b
Steven Koska <stevenkoska2000@gmail.com>
2020-01-28 08:12:03 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var superheroes = require('superheroes');
+
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
+var supervillain = require('supervillain');
+
+var mySupervillainName = supervillain.random();
+console.log(mySupervillainName);
